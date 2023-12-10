aa87c48e4d15c23db2c96056959aacf2ee017c37
juanevilla <juan.evilla@hotmail.com>
2023-12-09 21:04:51 -0500

    lastenvivo

--- app/controllers/statistics_controller.py
+++ app/controllers/statistics_controller.py
@@ -11,65 +11,117 @@ from datetime import datetime, timedelta
 
 class StatisticsController:
     def __init__(self):
+        self.db = None
+
+    def get_db(self):
         self.db = get_db_connection()
+        return self.db
+
+    def close_db(self):
+        if self.db is not None:
+            self.db.close()
 
     def get_total_users(self):
-        return self.db.query(User).count()
+        db = self.get_db()
+        try:
+            return db.query(User).count()
+        finally:
+            self.close_db()
 
     def get_active_job_offers(self):
-        return self.db.query(JobOffer).filter(JobOffer.status == True).count()
+        db = self.get_db()
+        try:
+            return db.query(JobOffer).filter(JobOffer.status == True).count()
+        finally:
+            self.close_db()
 
     def get_companies(self):
-        return self.db.query(Company).limit(5).all()
+        db = self.get_db()
+        try:
+            return db.query(Company).limit(5).all()
+        finally:
+            self.close_db()
 
     def get_total_job_offers(self):
-        return self.db.query(JobOffer).count()
+        db = self.get_db()
+        try:
+            return db.query(JobOffer).count()
+        finally:
+            self.close_db()
 
     def get_new_job_offers_last_month(self):
-        one_month_ago = datetime.now() - timedelta(days=30)
-        total_job_offers = self.db.query(JobOffer).count()
-        new_job_offers = self.db.query(JobOffer).filter(JobOffer.status == True, JobOffer.creationdate >= one_month_ago).count()
-        return (new_job_offers / total_job_offers) * 100 if total_job_offers else 0
+        db = self.get_db()
+        try:
+            one_month_ago = datetime.now() - timedelta(days=30)
+            total_job_offers = db.query(JobOffer).count()
+            new_job_offers = db.query(JobOffer).filter(JobOffer.status == True, JobOffer.creationdate >= one_month_ago).count()
+            return (new_job_offers / total_job_offers) * 100 if total_job_offers else 0
+        finally:
+            self.close_db()
 
     def get_new_users_last_month(self):
-        one_month_ago = datetime.now() - timedelta(days=30)
-        total_users = self.db.query(User).count()
-        new_users = self.db.query(User).filter(User.creationdate >= one_month_ago).count()
-        return (new_users / total_users) * 100 if total_users else 0
+        db = self.get_db()
+        try:
+            one_month_ago = datetime.now() - timedelta(days=30)
+            total_users = db.query(User).count()
+            new_users = db.query(User).filter(User.creationdate >= one_month_ago).count()
+            return (new_users / total_users) * 100 if total_users else 0
+        finally:
+            self.close_db()
 
     def get_users_applied_to_jobs_count(self):
-        return self.db.query(User).join(Applicant, User.userid == Applicant.userid).distinct().count()
+        db = self.get_db()
+        try:
+            return db.query(User).join(Applicant, User.userid == Applicant.userid).distinct().count()
+        finally:
+            self.close_db()
 
     def get_total_companies(self):
-        return self.db.query(Company).count()
+        db = self.get_db()
+        try:
+            return db.query(Company).count()
+        finally:
+            self.close_db()
 
     def get_job_offers_by_year(self):
-        current_year = datetime.now().year
-        last_year = current_year - 1
-        job_offers_current_year = [0]*12
-        job_offers_last_year = [0]*12
-        job_offers = self.db.query(JobOffer).all()
-        for job_offer in job_offers:
-            if job_offer.creationdate.year == current_year:
-                job_offers_current_year[job_offer.creationdate.month - 1] += 1
-            elif job_offer.creationdate.year == last_year:
-                job_offers_last_year[job_offer.creationdate.month - 1] += 1
-        return job_offers_current_year, job_offers_last_year
+        db = self.get_db()
+        try:
+            current_year = datetime.now().year
+            last_year = current_year - 1
+            job_offers_current_year = [0]*12
+            job_offers_last_year = [0]*12
+            job_offers = db.query(JobOffer).all()
+            for job_offer in job_offers:
+                if job_offer.creationdate.year == current_year:
+                    job_offers_current_year[job_offer.creationdate.month - 1] += 1
+                elif job_offer.creationdate.year == last_year:
+                    job_offers_last_year[job_offer.creationdate.month - 1] += 1
+            return job_offers_current_year, job_offers_last_year
+        finally:
+            self.close_db()
 
     def get_users_by_roles(self):
-        roles = self.db.query(Role).all()
-        users_by_roles = {}
-        for role in roles:
-            users_by_roles[role.name] = self.db.query(User).join(User.roles).filter(Role.name == role.name).count()
-        return users_by_roles
-    
+        db = self.get_db()
+        try:
+            roles = db.query(Role).all()
+            users_by_roles = {}
+            for role in roles:
+                users_by_roles[role.name] = db.query(User).join(User.roles).filter(Role.name == role.name).count()
+            return users_by_roles
+        finally:
+            self.close_db()
+
     def get_roles_count(self):
-        roles = self.db.query(Role).all()
-        roles_count = {}
-        for role in roles:
-            role_users_count = self.db.query(User).join(User.roles).filter(Role.roleid == role.roleid).count()
-            roles_count[role.name] = role_users_count
-        return roles_count
+        db = self.get_db()
+        try:
+            roles = db.query(Role).all()
+            roles_count = {}
+            for role in roles:
+                role_users_count = db.query(User).join(User.roles).filter(Role.roleid == role.roleid).count()
+                roles_count[role.name] = role_users_count
+            return roles_count
+        finally:
+            self.close_db()
 
     def get_user_statistics(self):
         stats = {
