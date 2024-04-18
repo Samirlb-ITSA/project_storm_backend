5dc8b7045d93c8c697ac3e5e221b1ce374893dac
Samirlb-ITSA <selora@itsa.edu.co>
2024-04-17 21:00:04 -0500

    feat: add company name and address

--- app/controllers/job_offer_controller.py
+++ app/controllers/job_offer_controller.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException
 from config.db_config import get_db_connection
 from models.job_offer_model import JobOffer, JobOfferIn
+from models.company_model import Company
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import joinedload
@@ -26,7 +27,23 @@ class JobOfferController:
             job_offer = db.query(JobOffer).options(joinedload(JobOffer.applicants)).filter(JobOffer.offerid == job_offer_id).first()
             if job_offer is None:
                 return {"resultado": "Oferta no encontrada"}
-            return jsonable_encoder(job_offer)
+            
+            job_offer = jsonable_encoder(job_offer)
+            job_offer_dict = {
+                "offerid": str(job_offer.offerid),
+                "name": job_offer.name,
+                "workday": job_offer.workday,
+                "status": job_offer.status,
+                "creationdate": job_offer.creationdate,
+                "companyid": str(job_offer.companyid),
+                "companyname": "Company Name",  # Set companyname from backend
+                "companyaddress": "Company Address"  # Set companyaddress from backend
+            }
+
+            company = db.query(Company).filter(Company.companyid == job_offer["companyid"]).first()
+            job_offer_dict["companyname"] = company.name
+            job_offer_dict["companyaddress"] = company.address
+            return job_offer_dict
         finally:
             db.close()
 
@@ -36,9 +53,32 @@ class JobOfferController:
             job_offers = db.query(JobOffer).options(joinedload(JobOffer.applicants)).all()
             if not job_offers:
                 return {"resultado": "No se encontraron ofertas"}
-            return {"resultado": jsonable_encoder(job_offers)}
+            
+            job_offers_list = []
+            for job_offer in job_offers:
+                company = db.query(Company).filter(Company.companyid == job_offer.companyid).first()
+                if company:
+                    companyname = company.name
+                else:
+                    companyname = "Company Name Not Found"
+                
+                job_offer_dict = {
+                    "offerid": str(job_offer.offerid),
+                    "name": job_offer.name,
+                    "workday": job_offer.workday,
+                    "status": job_offer.status,
+                    "creationdate": job_offer.creationdate,
+                    "companyid": str(job_offer.companyid),
+                    "companyname": companyname,
+                    "companyaddress": company.address  # You can fetch companyaddress similarly if needed
+                }
+                
+                job_offers_list.append(job_offer_dict)
+            
+            return {"resultado": job_offers_list}
         finally:
             db.close()
+
 
     def update_job_offer(self, job_offer: JobOfferIn):
         db = get_db_connection()
